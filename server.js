deff4a07f51d41ba7cba96caffb3e5cc91a29719
Gord Bolli <gord@bolli.ca>
2022-09-10 09:41:00 -0700

    Set up logger (morgan)

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/database')
+const logger = require('morgan')
 
+// Use dotenv file from config folder
 require('dotenv').config({ path: './config/.env' })
 
+// Connect to database
 connectDB()
+
+// Logging
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + `/index.html`)
 })
 
+// Run server
 app.listen(process.env.PORT, () => {
     console.log(`Running on port ${process.env.PORT}`)
 })
