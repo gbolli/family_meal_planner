088d520515910be8d9f0177ae6c53c3428fa5560
Gord Bolli <gord@bolli.ca>
2022-09-10 09:37:41 -0700

    Setup connection to database (mongo atlas)

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/database')
 
 require('dotenv').config({ path: './config/.env' })
+
+connectDB()
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + `/index.html`)
