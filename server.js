3201322d5a131967541ea6c5e8059625390d0d7e
Gord Bolli <gord@bolli.ca>
2022-09-10 09:19:49 -0700

    Set up config/.env

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+require('dotenv').config({ path: './config/.env' })
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + `/index.html`)
 })
 
-app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Running on port ${process.env.PORT}`)
 })
