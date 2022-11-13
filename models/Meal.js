37142ff1cb414df64ad1f16199a82750a8f973d4
Gord Bolli <gord@bolli.ca>
2022-11-12 21:47:11 -0800

    Create Meal Model

---
+++ models/Meal.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const MealSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  link: {
+    type: URL,
+    require: false
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model("Meal", MealSchema)
