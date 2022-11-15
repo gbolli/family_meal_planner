e9c7e3912281f19c2c87436d1a78366087b71366
Gord Bolli <gord@bolli.ca>
2022-11-15 09:35:24 -0800

    Add getMeal, getMeals to Meal controller

--- controllers/meal.js
+++ controllers/meal.js
@@ -1,5 +1,20 @@
+const Meal = require('../models/Meal')
+
 module.exports = {
-    getMeal: (req, res) => {
-        res.render(`meal.ejs`)
+    getMeal: async (req, res) => {
+        try {
+            const meal = await Meal.findById(req.params.id)
+            res.render(`meal.ejs`, { meal: meal })
+        } catch (error) {
+            console.error(error)
+        }
     },
+    getMeals: async (req, res) => {
+        try {
+            const meals = await Meal.find()
+            res.render(`meals.ejs`, { meals: meals })
+        } catch (error) {
+            console.error(error)
+        }
+    }
 }
